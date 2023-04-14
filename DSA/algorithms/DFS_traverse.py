149ccb96f66b4d8a39096ace46d7224bb61c4594
carlosmab <carlosm.araujob@gmail.com>
2023-04-14 12:33:24 -0500

    DFS

--- DSA/algorithms/DFS_traverse.py
+++ DSA/algorithms/DFS_traverse.py
@@ -1,6 +1,6 @@
-# Traversals
-# Visiting every single node
-# O(n)
+# InOrder = [1, 4, 6, 9, 15, 20, 170]
+# PreOrder = [9, 4, 20, 1, 6, 15, 170]
+# PostOrder = [1, 6, 4, 15, 170, 20, 9]
 
 from performance import timer
 from binary_search import BinaryTree, Node
@@ -23,17 +23,6 @@ def breadthFirstSearch(tree: BinaryTree) -> list:
     return search_list
 
 
-def breadthFirstSearchRecursive(queue: list = [], search_list: list = []) -> list:
-    if not queue: return search_list
-    
-    current_node = queue.pop(0)
-    search_list.append(current_node.value)
-    
-    if current_node.left:
-        queue.append(current_node.left)
-    if current_node.right:
-        queue.append(current_node.right)
-    return breadthFirstSearchRecursive(queue, search_list)
 
 
 if __name__ == "__main__":
@@ -47,10 +36,7 @@ if __name__ == "__main__":
 
     with timer("BFS"):
         print(breadthFirstSearch(bt))
-        
-    with timer("\nRecursive"):
-        queue = [bt.root]
-        print(breadthFirstSearchRecursive(queue, []))
+
 
     # to return BFS [9, 4, 20, 1, 6, 15, 170]
         
